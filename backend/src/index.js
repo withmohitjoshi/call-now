8982171d4b79dd513e02b1228ea1f68fa802c2e9
Mohit <mohit.joshi@capanicus.com>
2023-11-27 09:26:01 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173/"
+    origin: "*",
   },
 });
 
@@ -18,15 +18,6 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
-
-  socket.on("message", (message) => {
-    console.log("Message received:", message);
-    io.emit("message", message); // Broadcast the message to all connected clients
-  });
 });
 
 server.listen(3000, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ const App = () => {
   socket.on("connection", () => {
     console.log("Connected to Socket.IO server");
   });
-
-  socket.on("message", (message) => {
-    console.log("Message received:", message);
-  });
-
-  socket.emit("message", "Hello from client!");
+  
   return (
     <div className="flex items-center justify-center min-h-[100svh]">
       <div className="max-w-screen-sm shadow-xl rounded-xl px-4 py-6 mx-4">
