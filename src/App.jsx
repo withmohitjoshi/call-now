452dbb0ebdf6a7b910fe83cf3f2343f12ad3fbf3
Mohit <mohit.joshi@capanicus.com>
2023-12-14 17:27:18 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 import Dialpad from "./components/Dialpad";
-import { useSocketContext } from "./hooks/useSocketContext";
 
 const App = () => {
-  const socket = useSocketContext()
-
-  useEffect(() => {
-    socket.emit('msg', { name: "eohit" })
-  }, [])
-
   return (
     <div className="flex items-center justify-center min-h-[100svh]">
       <div className="max-w-screen-sm shadow-xl rounded-xl px-4 py-6 mx-4">
--- src/hooks/useSocketContext.jsx
+++ src/hooks/useSocketContext.jsx
@@ -1,11 +1,28 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect("http://localhost:3000");
 const SocketContext = createContext(socket);
 
 export default function SocketWrapper({ children }) {
-  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
 }
 
 export const useSocketContext = () => useContext(SocketContext);
+
+export const useEmitSocket = ({ eventName = "", data = null }) => {
+  return () => socket.emit(eventName, data);
+};
+
+export const useEmitSocketOnMount = ({ eventName = "", data = null }) => {
+  const ref = useRef(false);
+  useEffect(() => {
+    if (!ref.current) {
+      console.log('SUUUUUUUU')
+      socket.emit(eventName, data);
+      ref.current = true;
+    }
+  }, []);
+};
